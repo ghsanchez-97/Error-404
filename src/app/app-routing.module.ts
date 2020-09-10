f8b30263885d527db0e88f70622fcc2e00e00283
Guy Sanchez <guy.sanchez@nic.uni.edu.ni>
2020-09-10 16:28:58 -0600

    ghsanchez-97

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NopageComponent } from './Component/nopage/nopage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:NopageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopageComponent } from './Component/nopage/nopage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageComponent
   ],
   imports: [
     BrowserModule,
